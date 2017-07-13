91b1161038b7b1a9ddb90eaddfec196bd7f64f19
Shreyas Minocha <shreyasminocha7@gmail.com>
2017-07-13 12:33:58 +0530

    Remove unnecessary import

--- projects/06/HackAssembler/Parser.java
+++ projects/06/HackAssembler/Parser.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.IllegalStateException;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
